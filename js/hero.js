2ada7367246f38279db6af420dc5fe8ef25cf1ad
Joshua Wilkinson <joshuawilkinson@v1041-wn-rt-a-248-130.campus-dynamic.uwaterloo.ca>
2019-05-23 00:39:43 -0400

    spawns more balls

--- js/hero.js
+++ js/hero.js
@@ -196,7 +196,7 @@ function getDisOf(b1, b2){
 
 // add balls if there a little balls
 function addBallIfy(){
-    if(balls.length < 60){
+    if(balls.length < 100){
         balls.push(getRandomBall());
     }
 }
